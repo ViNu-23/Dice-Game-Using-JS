4fcb2b9329de46327aa8ee858bfcf8454dea8610
ViNu-23 <vijayvinu46@gmail.com>
2023-11-12 01:06:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+const reloader = document.querySelector('.reload').addEventListener('click',refreshPage);
+function refreshPage(){
+    window.location.reload();
+    console.log('he refresher');
+}
+//player1
+const randomNumber= Math.floor(Math.random()*6)+1;
+const diceNumber =`inverted-dice-${randomNumber}.png`;
+const diceImg = `dice/${diceNumber}`;
+const image = document.querySelector('.img1');
+image.setAttribute('src',diceImg);
+
+//player2
+const randomNumber2= Math.floor(Math.random()*6)+1;
+const diceNumber2 =`inverted-dice-${randomNumber2}.png`;
+const diceImg2 = `dice/${diceNumber2}`;
+const image2 = document.querySelector('.img2');
+image2.setAttribute('src',diceImg2);
+
+//result
+const errorDiv = document.createElement('div');
+errorDiv.className = 'winner';
+const effect = document.querySelector('.left-player')
+const effect2 = document.querySelector('.right-player')
+
+//effect
+if (randomNumber > randomNumber2) {
+  errorDiv.appendChild(document.createTextNode('Player 1 Won')); 
+  effect.style.backgroundColor=`#9AE6B4`;
+  effect2.style.backgroundColor=`#FEB2B2`;
+
+} else if (randomNumber < randomNumber2) {
+  errorDiv.appendChild(document.createTextNode('Player 2 Won')); 
+  effect2.style.backgroundColor=`#9AE6B4`;
+  effect.style.backgroundColor=`#FEB2B2`;
+
+} else {
+  errorDiv.appendChild(document.createTextNode('It\'s a tie!')); 
+  effect.style.backgroundColor=`#90CDF4`;
+  effect2.style.backgroundColor=`#90CDF4`;
+}
+
+//placement
+const mainDiv = document.querySelector('.main');
+mainDiv.parentNode.insertBefore(errorDiv, mainDiv);
+
